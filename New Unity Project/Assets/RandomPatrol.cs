8cf4c7f271b17dc9807389b0010bf4a57b439422
RanaElif <rek4593@gmail.com>
2021-02-19 22:22:14 +0300

    RandomPatrol script added

---
+++ New Unity Project/Assets/RandomPatrol.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPatrol : MonoBehaviour
+{
+    public GameObject target;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (target.activeInHierarchy)
+            {
+                target.SetActive(false);
+            }
+            else
+            {
+                target.SetActive(true);
+            }
+
+        }
+        
+    }
+}
+
